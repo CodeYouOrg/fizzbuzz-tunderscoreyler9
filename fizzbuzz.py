aa77ee0eabbe4dfcea0ec30e0353a2f00e68cd56
Tyler Skidmore <tyler.s9@proton.me>
2024-07-31 13:13:02 -0400

    Finished fizzbuzz assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,34 @@
-# add your code here
+# scope = range(1, 101)
+
+# for num in scope:
+#     if num % 3 == 0 and num % 5 == 0:
+#         print('Fizzbuzz')
+#     elif num % 3 == 0:
+#         print('Fizz')
+#     elif num % 5 == 0:
+#         print('Buzz')
+#     else: print(num)
+
+def fizzbuzz(start, end):
+    for num in range(start, end + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            print('Fizzbuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
+
+def main():
+    print(fizzbuzz(1, 100))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
